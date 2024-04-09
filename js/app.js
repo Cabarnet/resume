27a7a556106e209fc2523b91473ac286a0a2e2b4
Ахметов Руслан <cabarnet713@gmail.com>
2024-04-10 01:55:59 +0300

    Update

--- js/app.js
+++ js/app.js
@@ -34,14 +34,21 @@ window.addEventListener('resize', () => {
     layers_container.style.cssText = `--vh: ${vh}px`;
 });
 
-function toLeft () {
-    let layer_1 = document.querySelector(".layer-1");
-    let main_content = document.querySelector(".main-content");
-    let skills_content = document.querySelector(".skills-content");
 
+
+let layer_1 = document.querySelector(".layer-1");
+let layer_2 = document.querySelector(".layer-2");
+let main_content = document.querySelector(".main-content");
+let skills_content = document.querySelector(".skills-content");
+let exp_content = document.querySelector(".exp-content");
+let about_content = document.querySelector(".about-content");
+let portfolio_content = document.querySelector(".portfolio-content");
+
+
+function toLeft () {
     main_content.style.cssText = 'display: none; opacity: 0';
-    layer_1.style.cssText = 'inset: -10vw -10vw -10vw -2vw';
-    // $(".layer-2").css({"inset": "-5vw -10vw -10vw -15vw", "transform": "scale(0.5) rotate(-5deg)", "opacity": 0.8, "filter": "blur(4px)"});
+    layer_1.style.cssText = 'inset: -10vw -10vw -10vw -2vw; filter: blur(4px)';
+    layer_2.style.cssText = 'inset: -5vw -10vw -10vw -15vw; transform: scale(0.5) rotate(-5deg); opacity: 0.8; filter: blur(4px)';
     skills_content.style.cssText = 'display: flex';
     anime({
         targets: '.skills-content .skills-content__el',
@@ -51,29 +58,21 @@ function toLeft () {
 }
 
 function toRight () {
-    let layer_1 = document.querySelector(".layer-1");
-    let main_content = document.querySelector(".main-content");
-    let music_content = document.querySelector(".music-content");
-
     main_content.style.cssText = 'display: none; opacity: 0';
-    layer_1.style.cssText = 'inset: -10vw -2vw -10vw -10vw';
-    // $(".layer-2").css({"inset": "-5vw -10vw -10vw -15vw", "transform": "scale(0.5) rotate(-5deg)", "opacity": 0.8, "filter": "blur(4px)"});
-    music_content.style.cssText = 'display: flex';
+    layer_1.style.cssText = 'inset: -10vw -2vw -10vw -10vw; filter: blur(4px)';
+    layer_2.style.cssText = 'inset: -5vw -10vw -10vw -15vw; transform: scale(0.5) rotate(-5deg); opacity: 0.8; filter: blur(4px)';
+    exp_content.style.cssText = 'display: flex';
     anime({
-        targets: '.music-content .music-content__el',
+        targets: '.exp-content .exp-content__el',
         opacity: 1,
         delay: anime.stagger([300, 1000])
     }); 
 }
 
 function toTop () {
-    let layer_1 = document.querySelector(".layer-1");
-    let main_content = document.querySelector(".main-content");
-    let about_content = document.querySelector(".about-content");
-
     main_content.style.cssText = 'display: none; opacity: 0';
-    layer_1.style.cssText = 'inset: 0vw -10vw -10vw -10vw';
-    // $(".layer-2").css({"inset": "-10vw -10vw -15vw -10vw", "transform": "scale(0.45) rotate(-5deg)", "opacity": 0.9, "filter": "brightness(70%)"});
+    layer_1.style.cssText = 'inset: 0vw -10vw -10vw -10vw; filter: brightness(70%)';
+    layer_2.style.cssText = 'inset: -10vw -10vw -10vw -12vw; transform: scale(0.52) rotate(-5deg); opacity: 0.9; filter: brightness(70%)';
     about_content.style.cssText = 'display: flex';
     anime({
         targets: '.about-content .about-content__el',
@@ -82,17 +81,12 @@ function toTop () {
         easing: 'easeInOutQuad',
         delay: anime.stagger([800, 250])
     });
-    
 }
 
 function toBottom () {  
-    let layer_1 = document.querySelector(".layer-1");
-    let main_content = document.querySelector(".main-content");
-    let portfolio_content = document.querySelector(".portfolio-content");
-
     main_content.style.cssText = 'display: none; opacity: 0';
-    layer_1.style.cssText = 'inset: -10vw -10vw 0vw -10vw';
-    // $(".layer-2").css({"inset": "-10vw -10vw -15vw -10vw", "transform": "scale(0.45) rotate(5deg)", "opacity": 0.9, "filter": "brightness(70%)"});
+    layer_1.style.cssText = 'inset: -10vw -10vw 0vw -10vw; filter: brightness(70%)';
+    layer_2.style.cssText = 'inset: -10vw -10vw -10vw -12vw; transform: scale(0.52) rotate(-5deg); opacity: 0.9; filter: brightness(70%)';
     portfolio_content.style.cssText = 'display: flex';
     anime({
         targets: '.portfolio-content .portfolio-content__el',
@@ -101,22 +95,15 @@ function toBottom () {
         easing: 'easeInOutQuad',
         delay: anime.stagger([800, 250])
     });
-    
 }
 
 function toMainPage () {
-    let layer_1 = document.querySelector(".layer-1");
-    let main_content = document.querySelector(".main-content");
-    let skills_content = document.querySelector(".skills-content");
-    let about_content = document.querySelector(".about-content");
-    let music_content = document.querySelector(".music-content");
-    let portfolio_content = document.querySelector(".portfolio-content");
-
     skills_content.style.cssText = 'display: none';
     about_content.style.cssText = 'display: none';
-    music_content.style.cssText = 'display: none';
+    exp_content.style.cssText = 'display: none';
     portfolio_content.style.cssText = 'display: none';
 
+    layer_2.style.cssText = 'transition: var(--transition-soft);';
     layer_1.style.cssText = 'inset: -10vw; transition: var(--transition-soft);';
     main_content.style.cssText = 'display: flex';
     anime({
@@ -127,16 +114,18 @@ function toMainPage () {
 }
 
 
-function openPortfolioWeather() {
-    let portfolio_content__el = document.querySelectorAll(".portfolio-content__el");
-    let portfolio_content_weather = document.querySelector("#weather");
-    let portfolio_close = document.querySelector(".portfolio-close");
 
+
+let portfolio_content__el = document.querySelectorAll(".portfolio-content__el");
+let portfolio_content_weather = document.querySelector("#weather");
+let portfolio_close = document.querySelector(".portfolio-close");
+
+function openPortfolioWeather() {
     portfolio_content__el.forEach(function(element) {
         element.style.display = 'none';
     });
 
-    portfolio_content_weather.style.cssText = portfolio_content_weather.getAttribute('style') + 'display: flex';
+    portfolio_content_weather.style.cssText = 'display: flex; opacity: 1; transform: translateY(250px);';
     portfolio_close.style.cssText = 'display: block';
     anime({
         targets: '.portfolio-close',
@@ -146,7 +135,12 @@ function openPortfolioWeather() {
 }
 
 function closePortfolio() {
-    $(".portfolio-content__el").css("display", "");
-    $("#weather").removeAttr('style');
-    $(".portfolio-close").removeAttr('style');
+    portfolio_content__el.forEach(function(element) {
+        element.style.display = '';
+    });
+    // $(".portfolio-content__el").css("display", "");
+    portfolio_content_weather.style.cssText = 'display: none';
+    // $("#weather").removeAttr('style');
+    portfolio_close.style.cssText = 'display: none';
+    // $(".portfolio-close").removeAttr('style');
 }
